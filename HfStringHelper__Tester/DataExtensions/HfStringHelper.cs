b3b98111f95280bf83e08947f22049216c5ca5c1
JUNGFOCUS7 <jungfocus7@gmail.com>
2023-06-06 00:28:59 +0900

    ``

---
+++ HfStringHelper__Tester/DataExtensions/HfStringHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+
+
+
+namespace HfStringHelper__Tester.DataExtensions
+{
+    public static class HfStringHelper
+    {
+/*
+        char tc = default;      //'\0'
+        string ts = default;    //null
+        int tn = default;       //0
+        float tf = default;     //0
+        double td = default;    //0
+        long tl = default;      //0
+        */
+
+
+
+        /// <summary>
+        /// 문자열에서 순서대로 숫자만 추출
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static string GetSeqNumStr(this string target)
+        {
+            if (string.IsNullOrWhiteSpace(target))
+                return string.Empty;
+
+            StringBuilder tsb = new StringBuilder();
+            foreach (char tc in target)
+            {
+                if (char.IsDigit(tc))
+                    tsb.Append(tc);
+            }
+
+            return tsb.ToString();
+        }
+
+
+        /// <summary>
+        /// 문자열에서 순서대로 숫자만 추출 (int)
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int GetSeqNum(this string target)
+        {
+            string ns = GetSeqNumStr(target);
+            if (int.TryParse(ns, out int rn))
+                return rn;
+            else
+                return default;
+        }
+
+    }
+
+}
---
+++ HfStringHelper__Tester/TesterProgram.cs
@@ -0,0 +1,54 @@
+﻿using HfStringHelper__Tester.DataExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace HfStringHelper__Tester
+{
+    public static class TesterProgram
+    {
+        public static void Main(string[] args)
+        {
+            //string nums = "0123456789abcdef";
+            //foreach (char tc in nums)
+            //{
+            //    Console.WriteLine($"char: {tc}, IsDigit: {char.IsDigit(tc)}, IsNumber: {char.IsNumber(tc)}");
+            //}
+
+            //string nums = "0123456789abcdef";
+            //for (int i = 0; i < int.MaxValue; ++i)
+            //{
+            //    char tc = (char)i;
+            //    if (char.IsDigit(tc) == char.IsNumber(tc))
+            //        continue;
+
+            //    string rst = $"char: {tc}, IsDigit: {char.IsDigit(tc)}, IsNumber: {char.IsNumber(tc)}";
+            //    Console.WriteLine(rst);
+            //}
+
+
+
+
+
+
+            string cs = "HB_923_L_72";
+            string rs = cs.GetSeqNumStr();
+            int rn = cs.GetSeqNum();
+
+            if (int.TryParse(null, out int tn))
+            {
+
+            }
+        }
+    }
+
+
+
+
+
+}
